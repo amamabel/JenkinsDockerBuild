58721d4cc9ff953a6be8b0fd036ee870f8007c88
Martin Abel <martin.abel@amazone.de>
2020-08-21 08:47:48 +0200

    Added a Nuget package for test purposes

--- JenkinsDockerBuild/Program.cs
+++ JenkinsDockerBuild/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,11 +9,32 @@ using System.Threading.Tasks;
 
 namespace JenkinsDockerBuild
 {
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("JENKINS DOCKER BUILD TEST APP " + FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion);
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
         }
     }
 }

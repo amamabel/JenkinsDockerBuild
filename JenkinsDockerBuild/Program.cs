5e3bad42d81a82b6bf063c891a10fc5cf0e982e2
Martin Abel <martin.abel@amazone.de>
2020-08-21 08:38:56 +0200

    Add project files.

---
+++ JenkinsDockerBuild/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDockerBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JENKINS DOCKER BUILD TEST APP " + FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion);
+        }
+    }
+}
